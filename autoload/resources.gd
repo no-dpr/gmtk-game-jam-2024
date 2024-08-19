41499fe635134f5f6125b07237613faa468aa6fd
Dominic Perno <dperno@mit.edu>
2024-08-18 23:02:47 -0700

    Implemented horizontal building system

---
+++ autoload/resources.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@export var stone_count := 0
+@export var wood_count := 0
+@export var skeleton_count := 7
+
+@export var sword_count := 0
+@export var bow_count := 0
+@export var hammer_count := 0
+
+@export var adventurers_killed := 0
